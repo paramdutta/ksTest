b0573531fdac579781b77d300cbfc371d69f6015
paramadutta <paramdutta@karmasphere.com>
2012-03-30 16:47:09 -0700

    Commit away ...

---
+++ src/KSTest.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: paramadutta
+ * Date: 3/30/12
+ * Time: 4:39 PM
+ *
+ *
+ * Life @ Karmasphere
+ */
+public class KSTest {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
